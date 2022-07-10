38e7d1d6f6936701ac175eb94ae14de9486587ac
Charlotte Natasha <natashaonyango97@gmail.com>
2022-07-10 13:06:29 +0200

    implemented api routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  name!: string;
+  image!: File; 
+
+  constructor(private http: HttpClient) { }
+
+  onTitleChanged(event: any){
+    this.name = event.target.value;
+  }
+
+  onImageChanged(event: any){
+    this.image= event.target.files[0];
+  }
+
+  newFood(){
+    const uploadData = new FormData();
+    uploadData.append('name', this.name);
+    uploadData.append('image', this.image, this.image.name);
+
+    this.http.post('http://127.0.0.1:8000/api/menu/', uploadData).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+      
+    );
+  }
 
   ngOnInit(): void {
   }
